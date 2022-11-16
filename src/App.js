0f4b4975d5081125ed8c10e0b663b9bfae28320e
tienphattran1103 <tienphattranssttt@gmail.com>
2022-11-16 20:43:05 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import { publicRoutes } from '~/routes';
-// import { DefaultLayout } from '~/components/Layouts/';
-
-// function App() {
-//     return (
-//         <Router>
-//             <div className="App">
-//                 <Routes>
-//                     {publicRoutes.map((route, index) => {
-//                         const Page = route.component;
-//                         return (
-//                             <Route
-//                                 key={index}
-//                                 path={route.path}
-//                                 element={
-//                                     <DefaultLayout>
-//                                         <Page />
-//                                     </DefaultLayout>
-//                                 }
-//                             />
-//                         );
-//                     })}
-//                 </Routes>
-//             </div>
-//         </Router>
-//     );
-// }
-
-// export default App;
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layouts/';
 
 function App() {
-    return <div> Hello</div>;
+    return (
+        <Router basename="/web-shop">
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <DefaultLayout>
+                                        <Page />
+                                    </DefaultLayout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
